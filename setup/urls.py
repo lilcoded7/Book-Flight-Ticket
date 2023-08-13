b9ec8d8acc3465545f2cbf6ebe297a2b0931ab10
coded <coded@gmail.com>
2023-08-13 02:00:37 -0400

    init

--- setup/urls.py
+++ setup/urls.py
@@ -25,7 +25,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1', include('ticket.urls')),
-    path('api/v1/account/', include('account.urls'))
+    path('api/v1/account/', include('account.urls')),
+    path("doc", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
